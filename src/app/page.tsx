caf69bbc5cf3c9e325e37f1923d5497654f6eebf
fabiolmorais <fabio.lmorais99@gmail.com>
2026-05-30 20:06:23 -0300

    Criado o componente container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 import SpinLoader from "@/components/SpinLoader";
 import { PostList } from "@/components/PostList";
 import { Suspense } from "react";
+import { Container } from "@/components/Container";
 
 export default async function HomePage() {
-
   return (
-    <div className="text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100">
+    <Container>
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>
-    </div>
+    </Container>
   );
 }

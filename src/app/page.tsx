a529fd4b74deeeb429eada4d9aca983b172b1e6f
fabiolmorais <fabio.lmorais99@gmail.com>
2026-05-30 20:18:41 -0300

    Criado componente Header responsivo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import SpinLoader from "@/components/SpinLoader";
 import { PostList } from "@/components/PostList";
 import { Suspense } from "react";
 import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
 
 export default async function HomePage() {
   return (
     <Container>
+      <Header />
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>

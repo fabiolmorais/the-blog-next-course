2f93191ff488c2b332fb2f65f66d25389e4c8271
fabiolmorais <fabio.lmorais99@gmail.com>
2026-05-31 12:54:20 -0300

    Componente FeaturedPost

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,48 @@
-import SpinLoader from "@/components/SpinLoader";
+import { Container } from "@/components/Container";
 import { PostList } from "@/components/PostList";
 import { Suspense } from "react";
-import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import SpinLoader from "@/components/SpinLoader";
+import Image from "next/image";
+import Link from "next/link";
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
+
+      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
+        <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
+          <Image
+            className="w-full h-full object-cover object-center group-hover:scale-105 transition"
+            src="/images/bryen_0.png"
+            width={1200}
+            height={720}
+            alt="Título do Post"
+            priority
+          ></Image>
+        </Link>
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time
+            className="text-slate-600 block text-sm/tight"
+            dateTime="2025-04-20"
+          >
+            20/04/2025 10:00
+          </time>
+
+          <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
+            <Link href="#">Lorem ipsum dolor sit amet consectetur</Link>
+          </h1>
+
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam aut
+            nam veniam deleniti itaque quod officia excepturi sint iste nisi
+            ipsum nihil obcaecati quibusdam incidunt, enim repudiandae veritatis
+            soluta nostrum?
+          </p>
+        </div>
+      </section>
+
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>

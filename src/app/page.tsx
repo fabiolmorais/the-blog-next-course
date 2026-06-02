d5ebef56b7cfc1f771b374931ba624ae4acbee96
fabiolmorais <fabio.lmorais99@gmail.com>
2026-06-01 21:51:00 -0300

    refatorado o page colocando o featured post dentro de um componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,4 @@
-import { PostCoverImage } from "@/components/PostCoverImage";
-import { PostHeading } from "@/components/PostHeading";
+import { PostFeatured } from "@/components/PostFeatured";
 import { Container } from "@/components/Container";
 import { PostList } from "@/components/PostList";
 import { Suspense } from "react";
@@ -11,40 +10,10 @@ export default async function HomePage() {
     <Container>
       <Header />
 
-      <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
-        <PostCoverImage
-          linkProps={{
-            href: "/post/asdasd",
-          }}
-          imageProps={{
-            width: 1200,
-            height: 720,
-            src: "/images/bryen_9.png",
-            alt: "Alt da imagem",
-            priority: true,
-          }}
-        />
-        <div className="flex flex-col gap-4 sm:justify-center">
-          <time
-            className="text-slate-600 block text-sm/tight"
-            dateTime="2025-04-20"
-          >
-            20/04/2025 10:00
-          </time>
-
-          <PostHeading as="h1" url="#">
-            Lorem ipsum dolor sit amet consectetur
-          </PostHeading>
-
-          <p>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quam aut
-            nam veniam deleniti itaque quod officia excepturi sint iste nisi
-            ipsum nihil obcaecati quibusdam incidunt, enim repudiandae veritatis
-            soluta nostrum?
-          </p>
-        </div>
-      </section>
-
+      <Suspense fallback={<SpinLoader />}>
+        <PostFeatured />
+      </Suspense>
+      
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>

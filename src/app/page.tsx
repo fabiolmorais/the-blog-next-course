41fd86193bd2114dd1823444940048f2a0425f91
fabiolmorais <fabio.lmorais99@gmail.com>
2026-06-02 21:54:45 -0300

    Criado o footer e ajustado o layout com metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,18 @@
 import { PostFeatured } from "@/components/PostFeatured";
-import { Container } from "@/components/Container";
 import { PostList } from "@/components/PostList";
 import { Suspense } from "react";
-import { Header } from "@/components/Header";
 import SpinLoader from "@/components/SpinLoader";
 
 export default async function HomePage() {
   return (
-    <Container>
-      <Header />
-
+    <>
       <Suspense fallback={<SpinLoader />}>
         <PostFeatured />
       </Suspense>
-      
+
       <Suspense fallback={<SpinLoader />}>
         <PostList />
       </Suspense>
-    </Container>
+    </>
   );
 }

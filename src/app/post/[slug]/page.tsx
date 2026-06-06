49a357e8de844e75da19874558ead511bc17d6c0
fabiolmorais <fabio.lmorais99@gmail.com>
2026-06-05 21:41:16 -0300

    refatorado a função findPostBySlugCached e criado a função generateMetadata

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,24 +1,26 @@
 import { findPostBySlugCached } from "@/lib/post/queries";
-import { notFound } from "next/navigation";
+import { Metadata } from "next";
 
 type PostSlugPageProps = {
   params: Promise<{ slug: string }>;
 };
 
+export async function generateMetadata({
+  params,
+}: PostSlugPageProps): Promise<Metadata> {
+  const { slug } = await params;
+  const post = await findPostBySlugCached(slug);
+
+  return {
+    title: post.title,
+    description: post.excerpt,
+  };
+}
+
 export default async function PostSlugPage({ params }: PostSlugPageProps) {
   const { slug } = await params;
+  const post = await findPostBySlugCached(slug);
 
-  let post;
-
-  try {
-    post = await findPostBySlugCached(slug);
-  } catch {
-    post = undefined;
-  }
-
-  if (!post) {
-    notFound();
-  }
   return (
     <div>
       <p>{post.content}</p>
